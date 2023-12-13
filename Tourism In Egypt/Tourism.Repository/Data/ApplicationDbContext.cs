8776cca0ab05cd0291b4b71191109f0219d3ee14
Sabry Gamal <sabrygamal976@gmail.com>
2023-12-13 10:06:08 +0200

    Project Preparation And Create Table Reviw And Notification

---
+++ Tourism In Egypt/Tourism.Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tourism.Models;
+
+namespace Talabat.Repository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+    }
+}

bad2588db58493d99c1c7e4ef817dbfdd3f2eecf
Arwa <arwaalaa24682468@gmail.com>
2024-03-19 22:47:52 +0200

    update

--- TourismMVC/Helpers/MappingProfiles.cs
+++ TourismMVC/Helpers/MappingProfiles.cs
@@ -7,7 +7,7 @@ namespace TourismMVC.Helpers
     public class MappingProfiles : Profile
     {
 
-<<<<<<< HEAD
+
 		public MappingProfiles()
 		{
 			CreateMap<PlaceViewModel,Place>().ReverseMap();
@@ -16,12 +16,7 @@ namespace TourismMVC.Helpers
 			CreateMap<Category, CategoryViewModel>().ReverseMap();
 
 			CreateMap<CityPhotosViewModel,CityPhotos>().ReverseMap();
-=======
-        public MappingProfiles()
-        {
-            CreateMap<PlaceViewModel, Place>().ReverseMap();
-            CreateMap<CityPhotosViewModel, CityPhotos>().ReverseMap();
->>>>>>> acb63b4454099ac197828b42ab48096f1c5d2400
+
             CreateMap<PlacePhotoViewModel, PlacePhotos>().ReverseMap();
             CreateMap<Place_TripModel, Place_Trip>().ReverseMap();
             CreateMap<RoleViewModel, ApplicationRole>()

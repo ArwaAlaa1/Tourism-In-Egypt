95fa7259afee1e2c63edf341e9418276ae9bf166
Arwa <arwaalaa24682468@gmail.com>
2024-06-19 05:41:43 +0300

    update

--- Tourism.Core/Repositories.Contract/IUnitOfWork.cs
+++ Tourism.Core/Repositories.Contract/IUnitOfWork.cs
@@ -5,7 +5,8 @@ namespace Tourism.Core.Repositories.Contract
     public interface IUnitOfWork<T> : IDisposable where T : BaseEntity
     {
         public IGenericRepository<T> generic { get; set; }
-        public IReviewRepository review { get; set; }
+	
+		public IReviewRepository review { get; set; }
         public IPlace_TripRepository placeTrip { get; set; }
         public IChangePassword changePassword { get; set; }
         int Complet();
